260372039b20b886ec9b053fa1f71fbf394a1aa0
Ikukubreeze <legiondesignsed@gmail.com>
2023-08-02 04:48:52 -0700

    typescripting

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,51 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+// student interrface with classes and data types
+
+const student1: Student = {
+  firstName: "Zeliq",
+  lastName: "Shannon",
+  age: 40,
+  location: "Nakuru",
+};
+// first student input
+
+const student2: Student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 37,
+  location: "Nairobi",
+};
+//  second student input
+
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement("table");
+const tbody = document.createElement("tbody");
+
+table.style.background = "pink";
+table.appendChild(tbody);
+
+studentsList.forEach((student: Student): void => {
+  const row = document.createElement("tr");
+  const nameCell = document.createElement("td");
+  const locationCell = document.createElement("td");
+
+  nameCell.textContent = student.firstName;
+  locationCell.textContent = student.location;
+
+  nameCell.style.border = "1px solid pink";
+  locationCell.style.border = "1px solid pink";
+  nameCell.style.padding = "5px";
+  locationCell.style.padding = "5px";
+
+  row.appendChild(nameCell);
+  row.appendChild(locationCell);
+  tbody.appendChild(row);
+});
+
+document.body.appendChild(table);
